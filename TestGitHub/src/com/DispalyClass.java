5d2f40ec2d5b721e417ce5ccc26d9fae4ecd1d82
NeelamPa <neelam.padwal@gmail.com>
2014-05-09 14:32:22 -0500

    Initial commit

---
+++ TestGitHub/src/com/DispalyClass.java
@@ -0,0 +1,10 @@
+package com;
+
+public class DispalyClass {
+	
+	public void Dispaly(){
+		
+		System.out.println("In display method");
+	}
+
+}
---
+++ TestGitHub/src/com/MainClass.java
@@ -0,0 +1,12 @@
+package com;
+
+public class MainClass {
+	
+
+
+	public static void main(String args[]){
+		DispalyClass dispalyClass = new DispalyClass();
+		System.out.println("In main method");
+		dispalyClass.Dispaly();
+	}
+}
